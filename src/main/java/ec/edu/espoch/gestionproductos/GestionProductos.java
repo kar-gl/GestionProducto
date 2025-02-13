e1a40ba2c6ae03a344445abd7d029297b1f1bb31
JhxnierCT <jhonier.cotaya02@gmail.com>
2025-02-13 13:59:42 -0500

    mi rama

---
+++ src/main/java/ec/edu/espoch/gestionproductos/GestionProductos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.gestionproductos;
+
+/**
+ *
+ * @author SO-LAB-PC5
+ */
+public class GestionProductos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
